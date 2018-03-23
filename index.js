f011f1289bc1d863df0f5f7e3a95c318acd63139
Emil Bay <github@tixz.dk>
2018-03-23 11:30:51 +0100

    Got the comments wrong on the RFC and tidier masking

--- index.js
+++ index.js
@@ -18,15 +18,13 @@ function create (buf) {
   // Mask then set bits
   // https://tools.ietf.org/html/rfc4122#section-4.1.2
 
+  // time_hi_and_version (MSB half)
+  // IETF variant
+  buf[6] = (buf[6] & 0b00001111) | 0b01000000
+
   // clock_seq_hi_and_reserved
   // version
-  buf[6] &= 0x0f // 0b00001111
-  buf[6] |= 0x40 // 0b01000000
-
-  // time_hi_and_version
-  // IETF variant
-  buf[8] &= 0x3f // 0b00111111
-  buf[8] |= 0x80 // 0b10000000
+  buf[8] = (buf[8] & 0b00111111) | 0b10000000
 
   return buf
 }
